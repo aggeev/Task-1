ade85c7b9342af11ecfa7e6e7475565a97b3d33b
aggeev <den.ageev.2014@mail.ru>
2021-12-22 11:30:28 +0300

    test

---
+++ task 1.py
@@ -0,0 +1,13 @@
+duration = int(input("введите количество секунд"))
+if duration < 60:
+    print(str(duration) + ' сек')
+elif duration < 3600:
+    print(str(duration // 60) + ' мин ' + str(duration % 60) + ' сек')
+elif duration < 3600 * 24:
+    print(duration // 3600, 'hr', duration % 3600 // 60, 'min', duration % 60, 'sec')
+else:
+    days = duration // (3600 * 24)
+    hours = duration % (3600 * 24) // 3600
+    mins = duration % 3600 // 60
+    sec = duration % 60
+    print(days, 'days', hours, 'hr', mins, 'min', sec, 'sec')
